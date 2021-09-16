0fb1b0c69431eec9d92b6c707e725112f6414200
Jimmy Vu <JMV3211@gmail.com>
2021-09-16 16:53:57 -0700

    Check for errors #5

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var car = {
-  direction: 'east'
+  direction: 'east',
+  xAxis: 0,
+  yAxis: 0
 };
 
 var carDOM = document.querySelector('.car');
@@ -7,6 +9,18 @@ var carDOM = document.querySelector('.car');
 document.addEventListener('keydown', carFunction);
 
 function carFunction(event) {
+  if (event.key === ' ') {
+    if (car.direction === 'north') {
+      setInterval(function () { car.yAxis--; carDOM.style = 'top: ' + car.yAxis.toString() + 'px; left: ' + car.xAxis.toString() + 'px;'; }, 16);
+    } else if (car.direction === 'south') {
+      setInterval(function () { car.yAxis++; carDOM.style = 'top: ' + car.yAxis.toString() + 'px; left: ' + car.xAxis.toString() + 'px;'; }, 16);
+    } else if (car.direction === 'west') {
+      setInterval(function () { car.xAxis--; carDOM.style = 'top: ' + car.yAxis.toString() + 'px; left: ' + car.xAxis.toString() + 'px;'; }, 16);
+    } else if (car.direction === 'east') {
+      setInterval(function () { car.xAxis++; carDOM.style = 'top: ' + car.yAxis.toString() + 'px; left: ' + car.xAxis.toString() + 'px;'; }, 16);
+    }
+
+  }
   if (event.key === 'w') {
     carDOM.className = 'north';
     car.direction = 'north';
