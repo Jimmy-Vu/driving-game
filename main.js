5ae65f68ff4c521a9e622b8de8490fae8251ec86
Jimmy Vu <JMV3211@gmail.com>
2021-09-16 17:14:11 -0700

    Check if car is started on space keydown and if it is, clear the interval #7

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
 var car = {
   direction: 'east',
   xAxis: 0,
-  yAxis: 0
+  yAxis: 0,
+  started: false
 };
 
 var carDOM = document.querySelector('.car');
 
 document.addEventListener('keydown', carFunction);
 
+var movementIntervalID;
+
 function carFunction(event) {
   if (event.key === ' ') {
-    if (car.direction === 'north') {
-      setInterval(carNorth, 16);
-    } else if (car.direction === 'south') {
-      setInterval(carSouth, 16);
-    } else if (car.direction === 'west') {
-      setInterval(carWest, 16);
-    } else if (car.direction === 'east') {
-      setInterval(carEast, 16);
+    if (car.started === true) {
+      clearInterval(movementIntervalID);
+      car.started = false;
+    } else {
+      if (car.direction === 'north') {
+        movementIntervalID = setInterval(carNorth, 16);
+      } else if (car.direction === 'south') {
+        movementIntervalID = setInterval(carSouth, 16);
+      } else if (car.direction === 'west') {
+        movementIntervalID = setInterval(carWest, 16);
+      } else if (car.direction === 'east') {
+        movementIntervalID = setInterval(carEast, 16);
+      }
     }
-
   }
   if (event.key === 'w') {
     carDOM.className = 'north';
@@ -42,16 +49,20 @@ function carFunction(event) {
 function carNorth() {
   car.yAxis--;
   carDOM.style = 'top: ' + car.yAxis.toString() + 'px; left: ' + car.xAxis.toString() + 'px;';
+  car.started = true;
 }
 function carSouth() {
   car.yAxis++;
   carDOM.style = 'top: ' + car.yAxis.toString() + 'px; left: ' + car.xAxis.toString() + 'px;';
+  car.started = true;
 }
 function carWest() {
   car.xAxis--;
   carDOM.style = 'top: ' + car.yAxis.toString() + 'px; left: ' + car.xAxis.toString() + 'px;';
+  car.started = true;
 }
 function carEast() {
   car.xAxis++;
   carDOM.style = 'top: ' + car.yAxis.toString() + 'px; left: ' + car.xAxis.toString() + 'px;';
+  car.started = true;
 }
